0f759ab7af81ef5fe316a4485be0980f76b2c1a0
Mostafa Zaher <mostafa.zaher@affectiva.com>
2017-08-10 21:17:00 +0200

    Add multi ad support to POC

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,17 @@
+
+
 class HomeController < ApplicationController
 
   def index
   end
 
   def launch
+    if params[:id] == "1"
+      @movie_id = "122931_female_25.mp4"
+    elsif params[:id] == "2"
+      @movie_id = "122921_female_29.mp4"
+    else
+      @movie_id = ""
+    end
   end
-
 end
